89ade7babb0006433c9b32e1d5299088ace3af7f
enesgenisyurek <112898286+enesgenisyurek@users.noreply.github.com>
2023-08-13 21:23:10 +0300

    Update sudoku.java

--- interview-example/sudoku.java
+++ interview-example/sudoku.java
@@ -1,3 +1,8 @@
+/*Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with numbers in such a way that each column, 
+each row, and each of the nine 3 × 3 sub-grids that compose the grid all contain all of the numbers from 1 to 9 one time.*/
+
+
+
 boolean solution(char[][] grid) {
     boolean[][] rows = new boolean[9][9];
         boolean[][] columns = new boolean[9][9];
